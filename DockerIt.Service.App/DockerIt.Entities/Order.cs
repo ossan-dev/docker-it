cd514b5d9f9bdd9dbc15e5642d0e3f0c76590704
Ivan Pesenti <ivan.pesenti@abenergie.it>
2021-09-20 11:23:31 +0200

    add entities proj

---
+++ DockerIt.Service.App/DockerIt.Entities/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DockerIt.Entities
+{
+    public class Order
+    {
+        public Order()
+        {
+            OrderLines = new HashSet<OrderLine>();
+        }
+
+        public int OrderID { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime ExpectedDeliveryDate { get; set; }
+
+        public ICollection<OrderLine> OrderLines { get; private set; }
+    }
+}
---
+++ DockerIt.Service.App/DockerIt.Entities/OrderLine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DockerIt.Entities
+{
+    public class OrderLine
+    {
+        public int OrderLineID { get; set; }
+        public int OrderID { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+
+        public Order Order { get; set; }
+    }
+}
